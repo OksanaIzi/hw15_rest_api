80e7b5de393b36e4d56b7d46bd74025dac7f7ea1
Oksana Izibekova <oksana.izibekova@mail.com>
2021-05-16 10:55:01 +0300

    добавила reqressin api tests

---
+++ src/test/java/ReqressInTests.java
@@ -0,0 +1,88 @@
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+
+public class ReqressInTests {
+
+    @BeforeAll
+    static void setup() {
+        RestAssured.baseURI = "https://reqres.in";
+    }
+
+    @Test
+    void successUsersListTest() {
+        given()
+                .when()
+                .get("/api/users?page=2")
+                .then()
+                .statusCode(200)
+                .body("data[0].first_name", equalTo("Michael"))
+                .body("data.find{it.email=='lindsay.ferguson@reqres.in'}.last_name",
+                        equalTo("Ferguson"));
+
+    }
+
+    @Test
+    void successSingleUser() {
+        given()
+                .when()
+                .get("/api/users/2")
+                .then()
+                .statusCode(200)
+                .body("data.id", is(2))
+                .body("data.email", is("janet.weaver@reqres.in"))
+                .body("support.url", is("https://reqres.in/#support-heading"));
+    }
+
+    @Test
+    void successUserCreate() {
+        given()
+                .contentType(ContentType.JSON)
+                .body("{ \"name\": \"morpheus\", \"job\": \"leader\" }")
+                .when()
+                .post("/api/users")
+                .then()
+                .statusCode(201)
+                .body("name", is("morpheus"));
+    }
+
+    @Test
+    void successUserLogin() {
+        given()
+                .contentType(ContentType.JSON)
+                .body("{ \"email\": \"eve.holt@reqres.in\", \"password\": \"cityslicka\" }")
+                .when()
+                .post("/api/login")
+                .then()
+                .statusCode(200)
+                .body("token", is("QpwL5tke4Pnpja7X4"));
+    }
+
+    @Test
+    void unsuccessDelete() {
+        given()
+                .when()
+                .delete("/api/users/2")
+                .then()
+                .statusCode(204);
+    }
+
+    @Test
+    void unsuccessUpdate() {
+        given()
+                .contentType(ContentType.JSON)
+                .body("{ \"name\": \"morpheus\", \"job\": \"zion resident\" }")
+                .when()
+                .put("/api/users/2")
+                .then()
+                .statusCode(200)
+                .body("name", equalTo("morpheus"))
+                .body("job", equalTo("zion resident"));
+    }
+
+}
